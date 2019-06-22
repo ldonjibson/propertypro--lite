7409e74195c1e5c2ca3784ee41d5a59983b4d78d
ldonjibson <ajibolaolayanju@gmail.com>
2019-06-22 22:41:21 +0100

    fix: add user route to the app.js

--- server/app.js
+++ server/app.js
@@ -3,12 +3,14 @@ import express from 'express';
 import path from 'path';
 import bodyparser from 'body-parser';
 import cors from 'cors';
+import routes from './route/index';
 
 const app = express();
 app.use(cors());
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({ extended: true }));
 app.use(express.static(path.resolve(__dirname, '../UI')));
+app.use('/api/v1/', routes);
 app.get('/', (req, res) => res.sendFile('../UI/index.html'));
 
 app.all('*', (req, res) => {
