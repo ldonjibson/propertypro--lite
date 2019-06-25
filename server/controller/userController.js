f318f4c04368336f03fa1280ecf3ef455c09bf6d
ldonjibson <ajibolaolayanju@gmail.com>
2019-06-25 21:00:27 +0100

    feat: signin endpoint and tests

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -19,24 +19,6 @@ class UserController {
    * @returns {object} Json
    * @memberof UserControllers
    */
-  static async register(req, res) {
-    let { firstName, lastName, email, password, phoneNumber, accountType, address } = req.body;
-    let newUser;
-    try {
-      const hashPassword = await PasswordManager.hashPassword(password);
-      const userDetails = await users.find(user => user.email === email);
-      if (userDetails) {
-        return response(res, 409, 'Email already in use');
-      }
-      let id; let isAdmin; let createdOn; let token;
-      [token, id, isAdmin, createdOn, password] = [`45erkjherht4549${Math.floor(Math.random() * 10000)}`, users.length + 1, false, Date.now(), hashPassword ];
-      newUser = { token, id, firstName, lastName, email, password, phoneNumber, address, accountType, isAdmin, createdOn}     
-      users.push(newUser);
-    } catch (error) {
-      return response(res, 500, 'Server error');
-    }
-    return response(res, 201, 'Successfully created a new user account', newUser);
-  }
 
   /**
    * @static signin
@@ -53,20 +35,20 @@ class UserController {
     try {
       userDetails = await users.find(user => user.email === email);
       if (!userDetails) {
-        return response(res, 400, 'User doesn\'t exist');
+        return response.errorResponse(res, 404, 'error', 'User doesn\'t exist');
       }
       isPasswordValid = PasswordManager.verifyPassword(password, userDetails.password);
       if (!isPasswordValid) {
-        return response(res, 400, 'Incorrect Password or Email');
+        return response.errorResponse(res, 400, 'error', 'Incorrect Password or Email');
       }
     } catch (error) {
-      return response(res, 500, 'Server error');
+      return response.errorResponse(res, 500, 'error', 'Server error');
     }
     delete userDetails.password;
     if (isPasswordValid) {
-      return response(res, 200, 'Successfully signed in', userDetails);
+      return response.successResponse(res, 200, 'success', userDetails);
     }
-    return response(res, 400, 'Invalid Password or Email');
+    return response.errorResponse(res, 400, 'error', 'Invalid Password or Email');
   }
 }
 
