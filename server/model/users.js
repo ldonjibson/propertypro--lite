8854534e3792087acdca58b7e60871daa8bb39d8
ldonjibson <ajibolaolayanju@gmail.com>
2019-06-30 21:45:51 +0100

    fix: change some propeties of user object from camel to snake case

--- server/model/users.js
+++ server/model/users.js
@@ -1,88 +1,88 @@
 const users = [
   {
     "id":1,
-    "firstName": 'Nairobi',
-    "lastName": 'Ozil',
+    "first_name": 'Nairobi',
+    "last_name": 'Ozil',
     "email": 'nairobi@gmail.com',
     "password": '$2a$08$QClsX3MmKFZEaPjD/wo6LOFopJo.fDtbgv3xfpRFCnl5ZE.ugsNlO',
     "phoneNumber": '234818103200055',
     "address": '22, owodunni, street, ikeja',
     "accountType": 'agent', // client or agent
-    "isAdmin": 'true', // must be a agent user account
+    "is_admin": true, 
     "createdOn": Date.now()
   },
   {
     "id":2,
-    "firstName": 'Berlini',
-    "lastName": 'jone',
+    "first_name": 'Berlini',
+    "last_name": 'jone',
     "email": 'Berliniike@gmail.com',
     "password": '$2a$08$QClsX3MmKFZEaPjD/wo6LOFopJo.fDtbgv3xfpRFCnl5ZE.ugsNlO',
     "phoneNumber": '234818103224000',
     "address": '22, owodunni, street, ikeja',
     "accountType": 'agent', // client or agent
-    "isAdmin": 'false', // must be a agent user account
+    "is_admin": false, 
     "createdOn": Date.now()
   },
   {
     "id":3,
-    "firstName": 'John',
-    "lastName": 'dunga',
+    "first_name": 'John',
+    "last_name": 'dunga',
     "email": 'johndum@gmail.com',
     "password": '$2a$08$QClsX3MmKFZEaPjD/wo6LOFopJo.fDtbgv3xfpRFCnl5ZE.ugsNlO',
     "phoneNumber": '234908103224055',
     "address":  '22, owodunni, street, ikeja',
     "accountType": 'client', // client or agent
-    "isAdmin": 'false', // must be a agent user account
+    "is_admin": false, 
     "createdOn": Date.now()
   },
   {
     "id":4,
-    "firstName": 'Mark',
-    "lastName": 'Rio',
+    "first_name": 'Mark',
+    "last_name": 'Rio',
     "email": 'MarkRio@gmail.com',
     "password": '$2a$08$QClsX3MmKFZEaPjD/wo6LOFopJo.fDtbgv3xfpRFCnl5ZE.ugsNlO',
     "phoneNumber": '234808003224055',
     "address": '22, owodunni, street, ikeja',
     "accountType": 'client', // client or agent
-    "isAdmin": 'false', // must be a agent user account
+    "is_admin": false, 
     "createdOn": Date.now()
   },
   {
     "id":5,
-    "firstName": 'Helsinki',
-    "lastName": 'soldier',
+    "first_name": 'Helsinki',
+    "last_name": 'soldier',
     "email": 'helsinki@gmail.com',
     "password": '$2a$08$QClsX3MmKFZEaPjD/wo6LOFopJo.fDtbgv3xfpRFCnl5ZE.ugsNlO',
     "phoneNumber": '234818103224055',
     "address": '22, owodunni, street, ikeja',
     "accountType": 'client', // client or agent
-    "isAdmin": 'false', // must be a agent user account
+    "is_admin": false, 
     "createdOn": Date.now()
   },
 
   {
     "id":6,
-    "firstName": 'Blessing',
-    "lastName": 'ashimi',
+    "first_name": 'Blessing',
+    "last_name": 'ashimi',
     "email": 'ashimi@gmail.com',
     "password": '$2a$08$QClsX3MmKFZEaPjD/wo6LOFopJo.fDtbgv3xfpRFCnl5ZE.ugsNlO',
     "phoneNumber": '234818122224455',
     "address": '22, owodunni, street, ikeja',
     "accountType": 'client', // client or agent
-    "isAdmin": 'false', // must be a agent user account
+    "is_admin": false, 
     "createdOn": Date.now()
   },
 
   {
     "id":7,
-    "firstName": 'Ajibola',
-    "lastName": 'Hussain',
+    "first_name": 'Ajibola',
+    "last_name": 'Hussain',
     "email": 'ajibolahussain@gmail.com',
     "password": '$2a$08$QClsX3MmKFZEaPjD/wo6LOFopJo.fDtbgv3xfpRFCnl5ZE.ugsNlO',
     "phoneNumber": '234808133224455',
     "address": '22, owodunni, street, ikeja',
     "accountType": 'agent', // client or agent
-    "isAdmin": 'false', // must be a agent user account
+    "is_admin": false, 
     "createdOn": Date.now()
   },
 ];
