acad062e1345f2bbde769854f937a29193a16067
ldonjibson <ajibolaolayanju@gmail.com>
2019-07-06 09:33:07 +0100

    chore(database): setup database connection and models to populate it

--- server/model/property.js
+++ server/model/property.js
@@ -3,129 +3,118 @@ const properties = [
   {
     "id": 1,
     "owner": 1, //
-    "type": 'mini flat', // 2bed room, mini flat
-    "status": 'available', // sold, available
-    "city": 'Lagos',
-    "city": 'Lagos',
-    "state": 'Lagos State',
-    "address": "29,owodunni street ilupeju onipanu",
+    "type": '\'mini flat\'', // 2bed room, mini flat
+    "status": '\'available\'', // sold, available
+    "city": '\'Lagos\'',
+    "state": '\'Lagos State\'',
+    "address": '\'29,owodunni street ilupeju onipanu\'',
     "price": 300000.00,
-    "created_on":Date.now(),
-    "image_url":"http://res.cloudinary.com/propertypro-lite/image/upload/v1561324469/iajxae68txspixfgsbrc.jpg"
+    "imageurl":'\'http://res.cloudinary.com/propertypro-lite/image/upload/v1561324469/iajxae68txspixfgsbrc.jpg\''
   },
   {
     "id": 2,
     "owner": 4, //
-    "type": '3 bed room', // 2bed room, mini flat
-    "status": 'dormant', // sold, available
-    "city": 'Lagos',
-    "state": 'Lagos State',
-    "address": "29,owodunni street ilupeju onipanu",
+    "type": '\'3 bed room\'', // 2bed room, mini flat
+    "status": '\'dormant\'', // sold, available
+    "city": '\'Lagos\'',
+    "state": '\'Lagos State\'',
+    "address": '\'29,owodunni street ilupeju onipanu\'',
     "price": 300000.00,
-    "created_on":Date.now(),
-    "image_url":"http://res.cloudinary.com/propertypro-lite/image/upload/v1561324469/iajxae68txspixfgsbrc.jpg"
+    "imageurl":'\'http://res.cloudinary.com/propertypro-lite/image/upload/v1561324469/iajxae68txspixfgsbrc.jpg\''
   },
   {
     "id": 3,
     "owner": 1, //
-    "type": '2 bed room', // 2bed room, mini flat
-    "status": 'sold', // sold, available
-    "city": 'Lagos',
-    "state": 'Lagos State',
-    "address": "29,owodunni street ilupeju onipanu",
+    "type": '\'2 bed room\'', // 2bed room, mini flat
+    "status": '\'sold\'', // sold, available
+    "city": '\'Lagos\'',
+    "state": '\'Lagos State\'',
+    "address": '\'29,owodunni street ilupeju onipanu\'',
     "price": 300000.00,
-    "created_on":Date.now(),
-    "image_url":"http://res.cloudinary.com/propertypro-lite/image/upload/v1561324469/iajxae68txspixfgsbrc.jpg"
+    "imageurl":'\'http://res.cloudinary.com/propertypro-lite/image/upload/v1561324469/iajxae68txspixfgsbrc.jpg\''
   },
   {
     "id": 4,
     "owner": 5, //
-    "type": '2 bed room', // 2bed room, mini flat
-    "status": 'available', // sold, available
-    "city": 'Lagos',
-    "state": 'Lagos State',
-    "address": "29,owodunni street ilupeju onipanu",
+    "type": '\'2 bed room\'', // 2bed room, mini flat
+    "status": '\'available\'', // sold, available
+    "city": '\'Lagos\'',
+    "state": '\'Lagos State\'',
+    "address": '\'29,owodunni street ilupeju onipanu\'',
     "price": 300000.00,
-    "created_on":Date.now(),
-    "image_url":"http://res.cloudinary.com/propertypro-lite/image/upload/v1561324469/iajxae68txspixfgsbrc.jpg"
+    "imageurl":'\'http://res.cloudinary.com/propertypro-lite/image/upload/v1561324469/iajxae68txspixfgsbrc.jpg\''
   },
 
   {
     "id": 5,
     "owner": 3, //
-    "type": '3 bed room', // 2bed room, mini flat
-    "status": 'sold', // sold, available
-    "city": 'Lagos',
-    "state": 'Lagos State',
-    "address": "29,owodunni street ilupeju onipanu",
+    "type": '\'3 bed room\'', // 2bed room, mini flat
+    "status": '\'sold\'', // sold, available
+    "city": '\'Lagos\'',
+    "state": '\'Lagos State\'',
+    "address": '\'29,owodunni street ilupeju onipanu\'',
     "price": 300000.00,
-    "created_on":Date.now(),
-    "image_url":"http://res.cloudinary.com/propertypro-lite/image/upload/v1561324469/iajxae68txspixfgsbrc.jpg"
+    "imageurl":'\'http://res.cloudinary.com/propertypro-lite/image/upload/v1561324469/iajxae68txspixfgsbrc.jpg\''
   },
 
   {
     "id": 6,
     "owner": 1, //
-    "type": '2 bed room', // 2bed room, mini flat
-    "status": 'sold', // sold, available
-    "city": 'Lagos',
-    "state": 'Lagos State',
-    "address": "29,owodunni street ilupeju onipanu",
+    "type": '\'2 bed room\'', // 2bed room, mini flat
+    "status": '\'sold\'', // sold, available
+    "city": '\'Lagos\'',
+    "state": '\'Lagos State\'',
+    "address": '\'29,owodunni street ilupeju onipanu\'',
     "price": 300000.00,
-    "created_on":Date.now(),
-    "image_url":"http://res.cloudinary.com/propertypro-lite/image/upload/v1561324469/iajxae68txspixfgsbrc.jpg"
+    "imageurl":'\'http://res.cloudinary.com/propertypro-lite/image/upload/v1561324469/iajxae68txspixfgsbrc.jpg\''
   },
 
   {
     "id": 7,
     "owner": 5, //
-    "type": '2 bed room', // 2bed room, mini flat
-    "status": 'available', // sold, available
-    "city": 'Lagos',
-    "state": 'Lagos State',
-    "address": "29,owodunni street ilupeju onipanu",
+    "type": '\'2 bed room\'', // 2bed room, mini flat
+    "status": '\'available\'', // sold, available
+    "city": '\'Lagos\'',
+    "state": '\'Lagos State\'',
+    "address": '\'29,owodunni street ilupeju onipanu\'',
     "price": 300000.00,
-    "created_on":Date.now(),
-    "image_url":"http://res.cloudinary.com/propertypro-lite/image/upload/v1561324469/iajxae68txspixfgsbrc.jpg"
+    "imageurl":'\'http://res.cloudinary.com/propertypro-lite/image/upload/v1561324469/iajxae68txspixfgsbrc.jpg\''
   },
 
   {
     "id": 8,
     "owner": 6, //
-    "type": '2 bed room', // 2bed room, mini flat
-    "status": 'available', // sold, available
-    "city": 'Lagos',
-    "state": 'Lagos State',
-    "address": "29,owodunni street ilupeju onipanu",
+    "type": '\'2 bed room\'', // 2bed room, mini flat
+    "status": '\'available\'', // sold, available
+    "city": '\'Lagos\'',
+    "state": '\'Lagos State\'',
+    "address": '\'29,owodunni street ilupeju onipanu\'',
     "price": 300000.00,
-    "created_on":Date.now(),
-    "image_url":"http://res.cloudinary.com/propertypro-lite/image/upload/v1561324469/iajxae68txspixfgsbrc.jpg"
+    "imageurl":'\'http://res.cloudinary.com/propertypro-lite/image/upload/v1561324469/iajxae68txspixfgsbrc.jpg\''
   },
 
   {
     "id": 9,
     "owner": 3, //
-    "type": '2 bed room', // 2bed room, mini flat
-    "status": 'available', // sold, available
-    "city": 'Lagos',
-    "state": 'Lagos State',
-    "address": "29,owodunni street ilupeju onipanu",
+    "type": '\'2 bed room\'', // 2bed room, mini flat
+    "status": '\'available\'', // sold, available
+    "city": '\'Lagos\'',
+    "state": '\'Lagos State\'',
+    "address": '\'29,owodunni street ilupeju onipanu\'',
     "price": 300000.00,
-    "created_on":Date.now(),
-    "image_url":"http://res.cloudinary.com/propertypro-lite/image/upload/v1561324469/iajxae68txspixfgsbrc.jpg"
+    "imageurl":'\'http://res.cloudinary.com/propertypro-lite/image/upload/v1561324469/iajxae68txspixfgsbrc.jpg\''
   },
 
   {
     "id": 10,
     "owner": 1, //
-    "type": '2 bed room', // 2bed room, mini flat
-    "status": 'sold', // sold, available
-    "city": 'Lagos',
-    "state": 'Lagos State',
-    "address": "29,owodunni street ilupeju onipanu",
+    "type": '\'2 bed room\'', // 2bed room, mini flat
+    "status": '\'sold\'', // sold, available
+    "city": '\'Lagos\'',
+    "state": '\'Lagos State\'',
+    "address": '\'29,owodunni street ilupeju onipanu\'',
     "price": 300000.00,
-    "created_on":Date.now(),
-    "image_url":"http://res.cloudinary.com/propertypro-lite/image/upload/v1561324469/iajxae68txspixfgsbrc.jpg"
+    "imageurl":'\'http://res.cloudinary.com/propertypro-lite/image/upload/v1561324469/iajxae68txspixfgsbrc.jpg\''
   },
 ];
 
--- server/model/users.js
+++ server/model/users.js
@@ -1,96 +1,82 @@
 const users = [
   {
-    "token": "45erkjherht45495783",
     "id":1,
-    "firstName": 'Nairobi',
-    "lastName": 'Ozil',
-    "email": 'nairobi@gmail.com',
-    "password": '$2a$08$QClsX3MmKFZEaPjD/wo6LOFopJo.fDtbgv3xfpRFCnl5ZE.ugsNlO',
-    "phoneNumber": '234818103200055',
-    "address": '22, owodunni, street, ikeja',
-    "accountType": 'agent', // client or agent
-    "isAdmin": 'true', // must be a agent user account
-    "createdOn": Date.now()
+    "firstname": '\'Nairobi\'',
+    "lastname": '\'Ozil\'',
+    "email": '\'nairobi@gmail.com\'',
+    "password": '\'$2a$08$QClsX3MmKFZEaPjD/wo6LOFopJo.fDtbgv3xfpRFCnl5ZE.ugsNlO\'',
+    "phonenumber": '\'234818103200055\'',
+    "address": '\'22, owodunni, street, ikeja\'',
+    "accounttype": '\'agent\'', // client or agent
+    "isadmin": '\'true\'', // must be a agent user account
   },
   {
-    "token": "45erkjherht45495784",
     "id":2,
-    "firstName": 'Berlini',
-    "lastName": 'jone',
-    "email": 'Berliniike@gmail.com',
-    "password": '$2a$08$QClsX3MmKFZEaPjD/wo6LOFopJo.fDtbgv3xfpRFCnl5ZE.ugsNlO',
-    "phoneNumber": '234818103224000',
-    "address": '22, owodunni, street, ikeja',
-    "accountType": 'agent', // client or agent
-    "isAdmin": 'false', // must be a agent user account
-    "createdOn": Date.now()
+    "firstname": '\'Berlini\'',
+    "lastname": '\'jone\'',
+    "email": '\'Berliniike@gmail.com\'',
+    "password": '\'$2a$08$QClsX3MmKFZEaPjD/wo6LOFopJo.fDtbgv3xfpRFCnl5ZE.ugsNlO\'',
+    "phonenumber": '\'234818103224000\'',
+    "address": '\'22, owodunni, street, ikeja\'',
+    "accounttype": '\'agent\'', // client or agent
+    "isadmin": '\'false\'', // must be a agent user account
   },
   {
-    "token": "45erkjherht45495785",
     "id":3,
-    "firstName": 'John',
-    "lastName": 'dunga',
-    "email": 'johndum@gmail.com',
-    "password": '$2a$08$QClsX3MmKFZEaPjD/wo6LOFopJo.fDtbgv3xfpRFCnl5ZE.ugsNlO',
-    "phoneNumber": '234908103224055',
-    "address":  '22, owodunni, street, ikeja',
-    "accountType": 'client', // client or agent
-    "isAdmin": 'false', // must be a agent user account
-    "createdOn": Date.now()
+    "firstname": '\'John\'',
+    "lastname": '\'dunga\'',
+    "email": '\'johndum@gmail.com\'',
+    "password": '\'$2a$08$QClsX3MmKFZEaPjD/wo6LOFopJo.fDtbgv3xfpRFCnl5ZE.ugsNlO\'',
+    "phonenumber": '\'234908103224055\'',
+    "address":  '\'22, owodunni, street, ikeja\'',
+    "accounttype": '\'client\'', // client or agent
+    "isadmin": '\'false\'', // must be a agent user account
   },
   {
-    "token": "45erkjherht45495786",
     "id":4,
-    "firstName": 'Mark',
-    "lastName": 'Rio',
-    "email": 'MarkRio@gmail.com',
-    "password": '$2a$08$QClsX3MmKFZEaPjD/wo6LOFopJo.fDtbgv3xfpRFCnl5ZE.ugsNlO',
-    "phoneNumber": '234808003224055',
-    "address": '22, owodunni, street, ikeja',
-    "accountType": 'client', // client or agent
-    "isAdmin": 'false', // must be a agent user account
-    "createdOn": Date.now()
+    "firstname": '\'Mark\'',
+    "lastname": '\'Rio\'',
+    "email": '\'MarkRio@gmail.com\'',
+    "password": '\'$2a$08$QClsX3MmKFZEaPjD/wo6LOFopJo.fDtbgv3xfpRFCnl5ZE.ugsNlO\'',
+    "phonenumber": '\'234808003224055\'',
+    "address": '\'22, owodunni, street, ikeja\'',
+    "accounttype": '\'client\'', // client or agent
+    "isadmin": '\'false\'', // must be a agent user account
   },
   {
-    "token": "45erkjherht45495787",
     "id":5,
-    "firstName": 'Helsinki',
-    "lastName": 'soldier',
-    "email": 'helsinki@gmail.com',
-    "password": '$2a$08$QClsX3MmKFZEaPjD/wo6LOFopJo.fDtbgv3xfpRFCnl5ZE.ugsNlO',
-    "phoneNumber": '234818103224055',
-    "address": '22, owodunni, street, ikeja',
-    "accountType": 'client', // client or agent
-    "isAdmin": 'false', // must be a agent user account
-    "createdOn": Date.now()
+    "firstname": '\'Helsinki\'',
+    "lastname": '\'soldier\'',
+    "email": '\'helsinki@gmail.com\'',
+    "password": '\'$2a$08$QClsX3MmKFZEaPjD/wo6LOFopJo.fDtbgv3xfpRFCnl5ZE.ugsNlO\'',
+    "phonenumber": '\'234818103224055\'',
+    "address": '\'22, owodunni, street, ikeja\'',
+    "accounttype": '\'client\'', // client or agent
+    "isadmin": '\'false\'', // must be a agent user account
   },
 
   {
-    "token": "45erkjherht45495788",
     "id":6,
-    "firstName": 'Blessing',
-    "lastName": 'ashimi',
-    "email": 'ashimi@gmail.com',
-    "password": '$2a$08$QClsX3MmKFZEaPjD/wo6LOFopJo.fDtbgv3xfpRFCnl5ZE.ugsNlO',
-    "phoneNumber": '234818122224455',
-    "address": '22, owodunni, street, ikeja',
-    "accountType": 'client', // client or agent
-    "isAdmin": 'false', // must be a agent user account
-    "createdOn": Date.now()
+    "firstname": '\'Blessing\'',
+    "lastname": '\'ashimi\'',
+    "email": '\'ashimi@gmail.com\'',
+    "password": '\'$2a$08$QClsX3MmKFZEaPjD/wo6LOFopJo.fDtbgv3xfpRFCnl5ZE.ugsNlO\'',
+    "phonenumber": '\'234818122224455\'',
+    "address": '\'22, owodunni, street, ikeja\'',
+    "accounttype": '\'client\'', // client or agent
+    "isadmin": '\'false\'', // must be a agent user account
   },
 
   {
-    "token": "45erkjherht45495780",
     "id":7,
-    "firstName": 'Ajibola',
-    "lastName": 'Hussain',
-    "email": 'ajibolahussain@gmail.com',
-    "password": '$2a$08$QClsX3MmKFZEaPjD/wo6LOFopJo.fDtbgv3xfpRFCnl5ZE.ugsNlO',
-    "phoneNumber": '234808133224455',
-    "address": '22, owodunni, street, ikeja',
-    "accountType": 'agent', // client or agent
-    "isAdmin": 'false', // must be a agent user account
-    "createdOn": Date.now()
+    "firstname": '\'Ajibola\'',
+    "lastname": '\'Hussain\'',
+    "email": '\'ajibolahussain@gmail.com\'',
+    "password": '\'$2a$08$QClsX3MmKFZEaPjD/wo6LOFopJo.fDtbgv3xfpRFCnl5ZE.ugsNlO\'',
+    "phonenumber": '\'234808133224455\'',
+    "address": '\'22, owodunni, street, ikeja\'',
+    "accounttype": '\'agent\'', // client or agent
+    "isadmin": '\'false\'', // must be a agent user account
   },
 ];
 export default users;
